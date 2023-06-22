dc103f0561ea3f3b2d99c6012c4d31afe3fdb564
Zaib Ali <zaibali615@gmail.com>
2023-06-22 19:54:08 +0530

    profile update

--- src/pages/main/Profile.js
+++ src/pages/main/Profile.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import ProfileLogo from "../../assets/images/profile-ph.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
 function Profile() {
@@ -8,18 +11,79 @@ function Profile() {
       <Navbar />
       <div className="profile-info">
         <div className="profile-box box">
-          <img src="./img.png" alt="" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
-            mollitia! Error exercitationem est alias quam aliquid, dolore atque
-            ducimus, voluptatum eveniet, voluptates rerum eos. Maiores
-            praesentium dignissimos corporis aperiam ratione eum,
-            necessitatibus, unde temporibus molestiae maxime voluptatibus
-            corrupti magni qui! Voluptates amet, esse deserunt sapiente
-            laboriosam eligendi quam ducimus laborum, dignissimos iste ipsam at
-            voluptas possimus dolor odio velit cumque excepturi dolore libero
-            dolorum.
-          </p>
+          <div className="top">
+            <h2>Profile</h2>
+            <FontAwesomeIcon icon={faPen} />
+          </div>
+          <div className="profile-data">
+            <img src={ProfileLogo} alt="Profile" />
+            <div className="profile-text">
+              <ul className="key">
+                <li className="key-li">
+                  <p className="keys">Full Name</p>
+                  <p>Username</p>
+                </li>
+                <li className="key-li">
+                  <p className="keys">ID</p>
+                  <p>@user222316</p>
+                </li>
+                <li className="key-li">
+                  <p className="keys">Location</p>
+                  <p>Bangalore, India</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="education-box box">
+          <div className="top">
+            <h2>About</h2>
+            <FontAwesomeIcon icon={faPen} />
+          </div>
+          <div className="profile-text">
+            <ul className="key">
+              <li className="key-li">
+                <p className="keys">Current Role</p>
+                <p>Student</p>
+              </li>
+              <hr />
+              <li className="key-li">
+                <p className="keys">Education</p>
+                <p>REVA University</p>
+              </li>{" "}
+              <hr />
+              <li className="key-li">
+                <p className="keys">Graduation Year</p>
+                <p>2025</p>
+              </li>{" "}
+              <hr />
+            </ul>
+          </div>
+        </div>
+        <div className="badge-box box">
+          <div className="top">
+            <h2>Badges</h2>
+            <FontAwesomeIcon icon={faPen} />
+          </div>
+          <div className="profile-text">
+            <ul className="key">
+              <li className="key-li">
+                <p className="keys">Current Role</p>
+                <p>Student</p>
+              </li>
+              <hr />
+              <li className="key-li">
+                <p className="keys">ID</p>
+                <p>@user222316</p>
+              </li>{" "}
+              <hr />
+              <li className="key-li">
+                <p className="keys">Location</p>
+                <p>Bangalore, India</p>
+              </li>{" "}
+              <hr />
+            </ul>
+          </div>
         </div>
       </div>
     </div>
