6bc5bed2b6d161d9befbcb5c96710ce00cbcee3e
Zaib Ali <zaibali615@gmail.com>
2023-06-22 15:43:22 +0530

    profile update

--- src/pages/main/Profile.js
+++ src/pages/main/Profile.js
@@ -6,19 +6,22 @@ function Profile() {
   return (
     <div className="profile-page">
       <Navbar />
-        <div className="profile-info">
-        <h1>Hello</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
-          mollitia! Error exercitationem est alias quam aliquid, dolore atque
-          ducimus, voluptatum eveniet, voluptates rerum eos. Maiores praesentium
-          dignissimos corporis aperiam ratione eum, necessitatibus, unde
-          temporibus molestiae maxime voluptatibus corrupti magni qui!
-          Voluptates amet, esse deserunt sapiente laboriosam eligendi quam
-          ducimus laborum, dignissimos iste ipsam at voluptas possimus dolor
-          odio velit cumque excepturi dolore libero dolorum.
-        </p></div>
- 
+      <div className="profile-info">
+        <div className="profile-box box">
+          <img src="./img.png" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+            mollitia! Error exercitationem est alias quam aliquid, dolore atque
+            ducimus, voluptatum eveniet, voluptates rerum eos. Maiores
+            praesentium dignissimos corporis aperiam ratione eum,
+            necessitatibus, unde temporibus molestiae maxime voluptatibus
+            corrupti magni qui! Voluptates amet, esse deserunt sapiente
+            laboriosam eligendi quam ducimus laborum, dignissimos iste ipsam at
+            voluptas possimus dolor odio velit cumque excepturi dolore libero
+            dolorum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
